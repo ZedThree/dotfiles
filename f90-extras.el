b2ca76f6afba683e246e9931a24a456d82f41e79
Peter Hill <zed.three@gmail.com>
2018-01-10 16:05:52 +0000

    Split fix formatting into -region and -procedure based functions

--- f90-extras.el
+++ f90-extras.el
@@ -1,44 +1,55 @@
-(defun f90-fix-formatting ()
-  "Fix some "
-  (interactive)
+(defun f90-fix-formatting-region (start end)
+  "Fix some common Fortran formatting problems in region"
+  (interactive "r")
   (save-excursion
     ;; Fix indentation
-    (f90-indent-subprogram)
+    (f90-indent-region start end)
 
     ;; Now only operate on this procedure
-    (f90-mark-subprogram)
-    (narrow-to-region (region-beginning) (region-end))
-    (deactivate-mark)
+    (narrow-to-region start end)
 
     ;; Fix uppercase keywords
-    (f90-mark-subprogram)
-    (f90-downcase-region-keywords (region-beginning) (region-end))
-    (deactivate-mark)
+    (f90-downcase-region-keywords start end)
+
+    ;; Remove unnecessary whitespace
+    (whitespace-cleanup-region start end)
 
     ;; Fix spaces around equals
-    (f90-beginning-of-subprogram)
+    (beginning-of-buffer)
     (while (re-search-forward "\\([^=*>/ ]\\)\\([-=+*/<>]\\|[<>/=]=\\)\\([^=* ]\\)" nil t)
       (replace-match "\\1 \\2 \\3"))
 
     ;; Fix spaces after commas
-    (f90-beginning-of-subprogram)
+    (beginning-of-buffer)
     (while (re-search-forward ",\\b" nil t)
       (replace-match ", "))
 
     ;; Fix multiple spaces before double-colon
-    (f90-beginning-of-subprogram)
+    (beginning-of-buffer)
     (while (re-search-forward " \\{2,\\}::" nil t)
       (replace-match " ::"))
 
     ;; Fix spaces around if statements
-    (f90-beginning-of-subprogram)
+    (beginning-of-buffer)
     (while (re-search-forward "if\\((.*?)\\)then" nil t)
       (replace-match "if \\1 then"))
 
     ;; Fix bad comments
-    (f90-beginning-of-subprogram)
+    (beginning-of-buffer)
     (while (search-forward "!#" nil t)
       (replace-match "!" nil t))
 
     ;; Back to whole buffer
     (widen)))
+
+(defun f90-fix-formatting-procedure ()
+  "Fix some common Fortran formatting problems in current procedure"
+  (interactive)
+  (save-excursion
+    (let ((start (progn
+                   (f90-beginning-of-subprogram)
+                   (point)))
+          (end (progn
+                 (f90-end-of-subprogram)
+                 (point))))
+      (f90-fix-formatting-region start end))))
